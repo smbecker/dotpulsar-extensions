064206961905b670bc66d945e24776c3bbd81471
Shaun Becker <smbecker@gmail.com>
2025-11-11 20:26:26 -0500

    Update ProtoBufSchema to be more generic

--- src/DotPulsar.Extensions.Schemas/Schemas/ProtoBufSchema.cs
+++ src/DotPulsar.Extensions.Schemas/Schemas/ProtoBufSchema.cs
@@ -27,31 +27,32 @@ public static class ProtoBufSchema
 /// </summary>
 /// <typeparam name="TMessage"></typeparam>
 /// <remarks>This still relies on the native Bytes schema rather than leveraging the official Pulsar schema registry.</remarks>
-public sealed class ProtoBufSchema<TMessage> : TypedSchema<TMessage> where TMessage : Google.Protobuf.IMessage<TMessage>
+public sealed class ProtoBufSchema<TMessage> : TypedSchema<TMessage>
 {
-	private readonly MessageParser<TMessage> parser;
+	private readonly MessageParser parser;
 
-	public ProtoBufSchema(MessageParser<TMessage> parser) {
+	public ProtoBufSchema(MessageParser parser) {
 		this.parser = parser ?? throw new ArgumentNullException(nameof(parser));
 	}
 
-	public ProtoBufSchema(MessageParser<TMessage> parser, ISchema<ReadOnlySequence<byte>> byteSchema)
+	public ProtoBufSchema(MessageParser parser, ISchema<ReadOnlySequence<byte>> byteSchema)
 		: base(byteSchema) {
 		this.parser = parser ?? throw new ArgumentNullException(nameof(parser));
 	}
 
 	protected override TMessage DecodeBytes(ReadOnlySequence<byte> bytes) {
-		return parser.ParseFrom(bytes);
+		return (TMessage)parser.ParseFrom(bytes);
 	}
 
 	protected override ReadOnlySequence<byte> EncodeBytes(TMessage message) {
-		var length = message.CalculateSize();
-		if (length <= 0) {
-			return ReadOnlySequence<byte>.Empty;
+		if (message is Google.Protobuf.IMessage m) {
+			var length = m.CalculateSize();
+			if (length > 0) {
+				var writer = new ArrayBufferWriter<byte>(length);
+				m.WriteTo(writer);
+				return new ReadOnlySequence<byte>(writer.WrittenMemory);
+			}
 		}
-
-		var writer = new ArrayBufferWriter<byte>(length);
-		message.WriteTo(writer);
-		return new ReadOnlySequence<byte>(writer.WrittenMemory);
+		return ReadOnlySequence<byte>.Empty;
 	}
 }
